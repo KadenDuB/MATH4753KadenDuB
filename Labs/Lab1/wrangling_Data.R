bb3a1671bf20d8ab97aac6b08e5a635fdf31e421
KadenDuB <KadenDuB31@gmail.com>
2024-08-28 13:41:48 -0500

    Completed Lab 2

--- Labs/Lab1/wrangling_Data.R
+++ Labs/Lab1/wrangling_Data.R
@@ -1,4 +1,12 @@
-ddt <- read.csv("DDT-1.csv")
+ddt <- read.csv("DDT.csv")
+I=ddt$WEIGHT
+z=(I-mean(I))/sd(I)
 
+# Outlier
+ddt$WEIGHT[abs(z) >= 3]
 
-with(ddt, ddt[,])
+# Possible Outlier
+ddt$WEIGHT[abs(z) >= 2 & abs(z) <= 3]
+
+#Sd(z)
+sd(z)
