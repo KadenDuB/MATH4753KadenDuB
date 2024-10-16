a04f9d4d69a1a8402c9d8df337e3ddef6fbff66c
KadenDuB <KadenDuB31@gmail.com>
2024-10-16 13:06:20 -0500

    Midterm Commit

--- InClass/3.R
+++
@@ -1,8 +0,0 @@
-mtbe <- read.csv('MTBE.csv')
-tble = table(mtbe$WellClass, mtbe$MTBE.Detect)
-head(tble)
-
-colSums(tble)
-rowSums(tble)
-sum(tble)
-
---
+++ InClass/InClass.R
@@ -0,0 +1,8 @@
+# Parameters
+mu <- 20
+sigma <- 6
+
+# Find P(Y > 20)
+1 - pnorm(20, mean = mu, sd = sigma)
+
+
